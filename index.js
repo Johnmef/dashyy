de130cb36ca059a6d00b9f70b85657b381320ca6
Johnmef <72965249+Johnmef@users.noreply.github.com>
2023-06-01 06:26:39 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 // API KEYS
+
+// weather
+const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=-37.987440,145.246058&days=1';
 const rapidApiKey = '5382c9f31dmsh152e2f8e48fa6dfp113c20jsnd40be36d07ab';
 const rapidApiHost = 'weatherapi-com.p.rapidapi.com'
+
+// amber electric
 const amberKey = 'psk_636774e0461a10b3bcc960532f51afae';
 const amberSite = '01EWZ1KPMH2F981DW2HMJB2B27';
+const amberUrl = `https://api.amber.com.au/v1/sites/${amberSite}/prices/current?next=5&previous=1&resolution=30`
+
+// timetree
 const authorizationToken = 'Bearer dLeYbhJ6iwCwjuCOHydLg0iNoOvlx9dd2ywrkNA_mAFc6stu';
 const calendarId = 'DGgPowYJxy4h';
 const apiUrl = `https://timetreeapis.com/calendars/${calendarId}/upcoming_events?timezone=Asia/Tokyo&days=1&include=creator`;
 
-
-
-// script.js
+(function(){
 const timeElement = document.getElementById("time");
 const dryerButton = document.getElementById("dryer-button");
 const washingSwitch = document.getElementById("washing-switch");
 
+setInterval(function() {
+    const date = new Date();
+    const time = date.toLocaleTimeString();
+    timeElement.textContent = time;
+}, 1000);
 
 let dryerState = 0; // Initial state of dryer button (0: off, 1: on)
-
 dryerButton.addEventListener("click", function() {
     dryerState = (dryerState === 0) ? 1 : 0;
     dryerButton.textContent = dryerState;
@@ -27,24 +37,36 @@ dryerButton.addEventListener("click", function() {
         dryerButton.style.backgroundColor = "#ccc";
     }
 });
+})()
 
-// Your existing code to update the time every second
-setInterval(function() {
-    const date = new Date();
-    const time = date.toLocaleTimeString();
-    timeElement.textContent = time;
-}, 1000);
+// This function tracks the time after the first execution to update data every fixed interval like 11:15, 11:30
+function updateMillisecs(minutes) {
+    const currentDate = new Date();
+  const currentMinutes = currentDate.getMinutes();
+  const currentSeconds = currentDate.getSeconds();
 
-const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=-37.987440,145.246058&days=1';
-const options = {
+  // Calculate the time until the next interval
+  const minutesUntilNextInterval = (minutes - 1) - (currentMinutes % minutes);
+
+  // Calculate the seconds until the next interval
+  const secondsUntilNextInterval = 60 - (currentSeconds);
+
+  // Calculate the milliseconds until the next interval
+  const millisecondsUntilNextInterval =
+    (minutesUntilNextInterval * 60 + secondsUntilNextInterval) * 1000;
+
+  return millisecondsUntilNextInterval;
+}
+
+// This function get the data from weather api
+function updateWeatherData() {
+  const options = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': rapidApiKey,
     'X-RapidAPI-Host': rapidApiHost
-  }
-};
-
-function updateWeatherData() {
+    }
+  };
   fetch(url, options)
     .then(response => response.json())
     .then(data => {
@@ -56,7 +78,6 @@ function updateWeatherData() {
           return hourValue >= currentTime;
         })
         .slice(0, 7);
-      console.log(futureHours)
       const maxDayTemp = data.forecast.forecastday[0].day.maxtemp_c;
       const minDayTemp = data.forecast.forecastday[0].day.mintemp_c;
 
@@ -77,16 +98,14 @@ function updateWeatherData() {
           icon = "🥶";
         }else if (text.includes("windy")){
           icon = "💨";
-        }else if (text.includes("cloudy")){
+        } else if (text.includes("cloudy")){
           icon = "☁️";
+        } else if (text.includes("overcast")){
+          icon = "☁️☁️";
         } else {
           icon = "🌡";
         }
-
-        console.log('Time:', formattedTime);
-        console.log('Temperature:', temperature);
         console.log('Text:', [text,icon]);
-        console.log('UV:', uv);
         console.log('----------------------');
 
         const placeholderBox = document.getElementById(`placeholder-box-${index+1}`);
@@ -98,7 +117,7 @@ function updateWeatherData() {
         timeElement.textContent = formattedTime;
         iconElement.textContent = `${icon}`;
         tempElement.textContent = `${temperature}°C`;
-        uvElement.textContent = `UV: ${uv}`;
+        // uvElement.textContent = `UV: ${uv}`;
 
       });
 
@@ -113,121 +132,82 @@ function updateWeatherData() {
           return hour - 12 + 'pm';
         }
       }
+      const millisecondsUntilNextInterval = updateMillisecs(30);
+
+    // Update the data every 30 minutes
+    setInterval(updateWeatherData, 30 * 60 * 1000);
+        console.log(`weather updated at ${new Date().toLocaleTimeString()}`)
+ 
     })
     .catch(error => {
       console.error(error);
     });
 }
 
-// Call the updateWeatherData function initially
-updateWeatherData();
+// This function get the data from amber api
+function displayEnergyPrices() {
+  const placeholderBoxes = document.querySelectorAll('.placeholder-boxx');
 
-// Update the weather data every hour
-setInterval(updateWeatherData, 60 * 60 * 1000); // 1 hour in milliseconds
-
-
-// Section 3 - Placeholder Boxes
-const placeholderBoxes = document.querySelectorAll('.placeholder-boxx');
-
-fetch(`https://api.amber.com.au/v1/sites/${amberSite}/prices/current?next=5&previous=1&resolution=30`, {
-  method: 'GET',
-  headers: {
-    'accept': 'application/json',
-    'Authorization': `Bearer ${amberKey}`
-  }
-})
-  .then(response => response.json())
-  .then(data => {
-    const currentDate = new Date();
-    const currentHour = currentDate.getHours();
-
-    // Adjust the first interval based on the current hour
-    if (currentHour === 2) {
-      data[0].startTime = currentDate.setMinutes(0) - 30 * 60000;
+  fetch(amberUrl, {
+    method: 'GET',
+    headers: {
+      'accept': 'application/json',
+      'Authorization': `Bearer ${amberKey}`
     }
-
-    // Iterate over the array
-    data.forEach((item, index) => {
-      const perKwh = item.perKwh;
-      const startTime = new Date(item.startTime);
-
-      // Convert interval start time to 12-hour format without leading zeros
-      const hours = startTime.getHours();
-      const minutes = startTime.getMinutes();
-      const period = hours >= 12 ? 'pm' : 'am';
-      const formattedHours = hours % 12 || 12;
-      const formattedMinutes = minutes === 0 ? '' : `:${minutes}`;
-
-      const placeholderBox = placeholderBoxes[index];
-      const timeElement = placeholderBox.querySelector('.time-placeholder');
-      const circleElement = placeholderBox.querySelector('.circle-placeholder');
-      const priceElement = placeholderBox.querySelector('.price-placeholder');
-
-      timeElement.textContent = `${formattedHours}${formattedMinutes}${period}`;
-      priceElement.textContent = `${Math.round(perKwh)} ¢`;
-
-      // Set color based on price range
-      if (perKwh < 24) {
-        circleElement.style.backgroundColor = 'limegreen';
-      } else if (perKwh >= 24 && perKwh <= 38) {
-        circleElement.style.backgroundColor = 'orange';
-      } else {
-        circleElement.style.backgroundColor = 'red';
-      }
-    });
   })
-  .catch(error => {
-    console.error(error);
-  });
+    .then(response => response.json())
+    .then(data => {
+      const currentDate = new Date();
+      const currentHour = currentDate.getHours();
+      // Adjust the first interval based on the current hour
+      if (currentHour === 2) {
+        data[0].startTime = currentDate.setMinutes(0) - 30 * 60000;
+      }
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+      // Iterate over the array
+      data.forEach((item, index) => {
+        const perKwh = item.perKwh;
+        const startTime = new Date(item.startTime);
+
+        // Convert interval start time to 12-hour format without leading zeros
+        const hours = startTime.getHours();
+        const minutes = startTime.getMinutes();
+        const period = hours >= 12 ? 'pm' : 'am';
+        const formattedHours = hours % 12 || 12;
+        const formattedMinutes = minutes === 0 ? '' : `:${minutes}`;
+
+        const placeholderBox = placeholderBoxes[index];
+        const timeElement = placeholderBox.querySelector('.time-placeholder');
+        const circleElement = placeholderBox.querySelector('.circle-placeholder');
+        const priceElement = placeholderBox.querySelector('.price-placeholder');
+
+        timeElement.textContent = `${formattedHours}${formattedMinutes}${period}`;
+        priceElement.textContent = `${Math.round(perKwh)} ¢`;
+
+        // Set color based on price range
+        if (perKwh <= 14) {
+          circleElement.style.backgroundColor = 'transparent';
+          circleElement.textContent = '🍀'
+        }else if (perKwh < 24) {
+          circleElement.style.backgroundColor = 'green';
+        } else if (perKwh >= 24 && perKwh <= 38) {
+          circleElement.style.backgroundColor = 'orange';
+        } else {
+          circleElement.style.backgroundColor = 'red';
+        }
+      });
+    
+    setInterval(displayEnergyPrices, 15 * 60 * 1000);
+      console.log(`electricity prices updated at ${new Date().toLocaleTimeString()}`)
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 
-
-fetch(apiUrl, {
-  headers: {
-    'Accept': 'application/vnd.timetree.v1+json',
-    'Authorization': authorizationToken
-  }
-})
-  .then(response => response.json())
-  .then(data => {
-    const events = data.data; // Array of events
-
-    // Extract title, start_at, and end_at for all events
-    const eventDetails = events.map(event => {
-      const { title, start_at, end_at } = event.attributes;
-
-      // Convert UTC time to Melbourne time
-      const startAtMelbourne = new Date(start_at).toLocaleString('en-AU', { timeZone: 'Australia/Melbourne' });
-      const endAtMelbourne = new Date(end_at).toLocaleString('en-AU', { timeZone: 'Australia/Melbourne' });
-
-      return { title, start_at: startAtMelbourne, end_at: endAtMelbourne };
-    });
-
-    // Log the event details
-    eventDetails.forEach(event => {
-      console.log('Title:', event.title);
-      console.log('Start At (Melbourne):', event.start_at);
-      console.log('End At (Melbourne):', event.end_at);
-      console.log('-----------------------');
-    });
-  })
-  .catch(error => {
-    console.error(error);
-  });
-
-// Section 4 - Calendar Events
-const section4 = document.getElementById('section2');
-
-// Fetch events from the calendar and display them in the section
 function displayCalendarEvents() {
+  const section4 = document.getElementById('section2');
+
   fetch(apiUrl, {
     headers: {
       'Accept': 'application/vnd.timetree.v1+json',
@@ -244,13 +224,13 @@ function displayCalendarEvents() {
 
         // Create event details elements
         const eventDiv = document.createElement('div');
-        eventDiv.classList. add('calender-events')
+        eventDiv.classList.add('calender-events');
         const titleElement = document.createElement('div');
         const timeElement = document.createElement('div');
-        const startAtMelbourne = new Date(start_at).toLocaleString('en-AU', { timeZone: 'Australia/Melbourne' })
-        const start = startAtMelbourne.slice(11,16 ) + startAtMelbourne.slice(19)
-      const endAtMelbourne = new Date(end_at).toLocaleString('en-AU', { timeZone: 'Australia/Melbourne' });
-        const end = endAtMelbourne.slice(11,16 ) + endAtMelbourne.slice(19)
+        const startAtMelbourne = new Date(start_at).toLocaleString('en-AU', { timeZone: 'Australia/Melbourne' });
+        const start = startAtMelbourne.slice(11, 16) + startAtMelbourne.slice(19);
+        const endAtMelbourne = new Date(end_at).toLocaleString('en-AU', { timeZone: 'Australia/Melbourne' });
+        const end = endAtMelbourne.slice(11, 16) + endAtMelbourne.slice(19);
 
         // Set event details
         titleElement.textContent = title;
@@ -263,17 +243,15 @@ function displayCalendarEvents() {
         // Append event div to section 4
         section4.appendChild(eventDiv);
       });
+    
+      setInterval(displayEnergyPrices, 120 * 60 * 1000);
+      console.log(`Timetree events updated at ${new Date().toLocaleTimeString()}`)
     })
     .catch(error => {
       console.error(error);
     });
 }
 
-// Call the function to display calendar events
-displayCalendarEvents();
-
-
-// fuel
 async function fetchProjectData() {
   try {
     const response = await fetch('https://projectzerothree.info/api.php?format=json');
@@ -284,12 +262,29 @@ async function fetchProjectData() {
 
     const fuelPriceElement = document.getElementById('fuel-price');
     fuelPriceElement.textContent = `\🚘 $${u91Price}`;
+    
+        
+    setInterval(displayEnergyPrices, 120 * 60 * 1000);
+    console.log(`Timetree events updated at ${new Date().toLocaleTimeString()}`)
 
   } catch (error) {
     console.error(error);
   }
 }
 
+// Call the displayCalendarEvents function initially
+displayCalendarEvents();
+// Call the updateWeatherData function initially
+updateWeatherData();
+// Call the displayEnergyPrices function initially
+displayEnergyPrices();
+// Call the fetchProjectData function initially
 fetchProjectData();
 
-// end fuel
+const millisecondsUntilNextInterval = updateMillisecs(15);
+
+setTimeout(() => {
+  displayEnergyPrices();
+  updateWeatherData();
+}, millisecondsUntilNextInterval);
+
